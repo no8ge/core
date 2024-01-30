f84733e9d81f288775673244773860527b0ee6da
lunzhou <lunzhou@yunify.com>
2024-01-30 18:59:00 +0800

    chore: remove hack

--- hack/docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-chart_appVersion=$(helm show chart chart | awk '/^appVersion:/ {print $2}')
-chart_name=$(helm show chart chart | awk '/^name:/ {print $2}')
-
-echo image name: $chart_name
-echo image tag: $chart_appVersion
-
-docker buildx build -f Dockerfile --platform linux/amd64 -t no8ge/$chart_name:$chart_appVersion . --push
