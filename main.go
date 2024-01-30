70d02cb4cf3e3c3cc912cdc4cb303554d7485599
lunzhou <lunzhou@yunify.com>
2024-01-30 19:02:29 +0800

    feat: cli mode

--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/no8ge/core/api"
-)
+import "github.com/no8ge/core/cmd"
 
 func main() {
-	r := gin.Default()
-
-	api.V1(r)
-
-	// r.Run() // listen and serve on 0.0.0.0:8080
-	r.RunTLS(":8080", "./certificates/core.pem", "./certificates/core.key")
+	cmd.Execute()
 }
